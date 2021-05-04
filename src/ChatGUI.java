0eb901f4ecda274f2b9ad18e53197de4b297d1ef
filip.norberg <filipnorberg02@outlook.com>
2021-05-04 13:38:47 +0200

    made my GUI design from my own sketch

--- src/ChatGUI.java
+++ src/ChatGUI.java
@@ -1,2 +1,24 @@
+import javax.swing.*;
+
 public class ChatGUI {
+    private JButton host;
+    private JButton connect;
+    private JButton send;
+    private JTextArea console;
+    private JTextArea message;
+    private JTextArea ip;
+    private JTextArea port;
+    private JPanel panel1;
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame ("ChatGUI");
+        frame.setContentPane(new ChatGUI().panel1);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
 }
